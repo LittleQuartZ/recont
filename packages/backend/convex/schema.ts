fe85753bc238e4103dd4d2bebced87096169eef5
Syahdan <syahdanhafizzz@gmail.com>
2025-07-26 05:08:29 +0700

    remove todos schema

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -2,11 +2,6 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  todos: defineTable({
-    text: v.string(),
-    completed: v.boolean(),
-  }),
-
   counters: defineTable({
     name: v.string(),
     count: v.number(),
